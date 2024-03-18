1e306d01eb341e761ea0c9b375750b2830d5965d
IvyBiancaGeronimo <geronimoivy07@gmail.com>
2024-03-18 09:25:19 +0800

    march 18 continuation

--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -6,3 +6,10 @@ require 'Database.php';
 require 'Model.php';
 require 'Controller.php';
 require 'App.php';
+
+spl_autoload_register(function ($class_name)
+{
+    require '../app/models/' . $class_name . '.php';
+
+
+});
