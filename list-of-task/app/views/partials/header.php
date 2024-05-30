e8ba3be57e191b2f2d6ee6fea0da84a73bc3cd66
jamildebelen26 <142187899+jamildebelen26@users.noreply.github.com>
2024-05-30 21:25:13 +0800

    Add files via upload

---
+++ list-of-task/app/views/partials/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Activity</title>
+  <link rel="stylesheet" href="<?= ROOT ?>/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?= ROOT ?>/assets/css/main.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">LIST OF TASK</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?= ROOT?>/tasks/index">Home</a>
+            </li>
+
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
